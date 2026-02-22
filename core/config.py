958ee810be4bd8d18e73b3f99f2ced6614adde37
umeshbongale <umeshbongale@gmail.com>
2026-02-22 10:22:16 +0530

    Update config.py
    updated config, provided .env file to load the config.

--- core/config.py
+++ core/config.py
@@ -1,8 +1,13 @@
-OLLAMA_URL = "http://localhost:11434"
-MODEL_NAME = "mistral:7b"
+import os
+from dotenv import load_dotenv
 
-META_ACCESS_TOKEN = "test"
-PHONE_NUMBER_ID = "98989877"
-VERIFY_TOKEN = "432432"
+load_dotenv()
 
-MAX_MEMORY = 6
+OLLAMA_URL = os.getenv("OLLAMA_URL", "http://localhost:11434")
+MODEL_NAME = os.getenv("MODEL_NAME", "mistral:7b")
+
+META_ACCESS_TOKEN = os.getenv("META_ACCESS_TOKEN")
+PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
+VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
+
+MAX_MEMORY = int(os.getenv("MAX_MEMORY", 6))
